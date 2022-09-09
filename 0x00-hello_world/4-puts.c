cb6e9aba256e138e629ae158ec6b0d76bb810571
Kushleo <kushleo419@gmail.com>
2022-09-09 11:27:43 -0700

    change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ * main - print the string in the put function
  *
- *   *
  *
- *    * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
- *     */
+ */
 
 int main(void)
 
 {
-            puts("\"Programming is like building a multilingual puzzle")
- 	    return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+                return (0);
 
 }
