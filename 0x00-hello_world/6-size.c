8832f58804cab3f2dafb8bc4ded8d508e6cac3ba
Kushleo <kushleo419@gmail.com>
2022-09-09 11:44:13 -0700

    change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,15 @@
 
 
 /**
- *
  * * main - print the string in the put function
  *
  * *
  *
- * * Description: using the main function
  *
  * * this program prints "Programming is like building a multilingual puzzle
  *
- * * Return: 0
- *
- * */
-
+ *  Return: 0
+ */
 int main(void)
 
 {
