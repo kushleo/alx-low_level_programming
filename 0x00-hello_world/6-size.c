061a8b5d08bf89e2bfa91694726055426e86481d
Kushleo <kushleo419@gmail.com>
2022-09-09 11:31:14 -0700

    change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
 
+
+
 /**
-
-* main - Prints the sizeof many var types
-
-*
-
-* Return: Always 0 (Success)
-
-*/
+ *
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ * */
 
 int main(void)
 
 {
 
-char a;
+		char c;
 
-int b;
+			int i;
 
-long int c;
+				long li;
 
-long long int d;
+					long long lli;
 
-float e;
+						float f;
 
-printf("Size of a char: %d byte(s)\n", sizeof(a));
 
-printf("Size of an int: %d byte(s)\n", sizeof(b));
 
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
+							printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+								printf("Size of an int: %ld byte(s)\n", sizeof(i));
 
-printf("Size of a float: %d byte(s)\n", sizeof(e));
+									printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 
-return (0);
+										printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 
+											printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+												return (0);
+
+}
