a253405c21049bfed8ef063698de1df1feff903a
Kushleo <kushleo419@gmail.com>
2022-09-09 06:50:42 -0700

    change

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,51 +3,24 @@
 
 
 /**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints #include <stdio.h>
- *
- *
- *
- */**
 
 * main - print the string in the put function
+
 *
-* *
-*
-* * Description: using the main function
-*
-* * this program prints with proper grammar, but the outcome is a piece of art,
-*
-* * Return: 0
-*
-* */
+
+* Description: using the main function
+
+* this program prints "Programming is like building a multilingual puzzle
+
+* Return: 0
+
+*/
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	return (0);
 
-}
- *
- * * Return: 0
- *
- * */
-
-int main(void)
-
-{
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
-}
