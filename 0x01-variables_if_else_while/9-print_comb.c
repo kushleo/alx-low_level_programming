284916acce905627f5b070e9ad8ff4b9ee6ddb6c
Kushleo <kushleo419@gmail.com>
2022-09-09 21:23:08 -0700

    change

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 
+
+
 /**
- *   * main - Prints a series of numbers with commas
- *
- *     Return: Always 0 (Success);
- */
+
+  * main - Prints a series of numbers with commas
+
+  *
+
+  * Return: Always (Success);
+
+  */
+
 int main(void)
+
 {
-int c;
+
+	int c;
+
+
 
 	for (c = '0'; c <= '9'; c++)
+
 	{
 
 		putchar(c);
-	 if (c != '9')
+
+
+
+		if (c != '9')
+
 		{
-			  putchar(',');
-			  putchar(' ');
-												}
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
 
 	}
 
+
+
 	putchar('\n');
+
+
+
 	return (0);
 
 }
+
