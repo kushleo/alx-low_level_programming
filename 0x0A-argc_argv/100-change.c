214ef5197fcabf02bce7c3905ee1c89adbbac3e3
Kushleo <kushleo419@gmail.com>
2022-10-01 12:20:29 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,34 +17,28 @@ int cents, coins = 0;
 if (argc != 2)
 {
 	printf("Error\n");
-
 	return (1);
 }
 
 cents = atoi(argv[1]);
-
 while (cents > 0)
 {
 	coins++;
-
 	if ((cents - 25) >= 0)
 	{
 		cents -= 25;
 		continue;
 	}
-
 	if ((cents - 10) >= 0)
 	{
 		cents -= 10;
 		continue;
 	}
-
 	if ((cents - 5) >= 0)
 	{
 		cents -= 5;
 		continue;
 	}
-
 	if ((cents - 2) >= 0)
 	{
 		cents -= 2;
@@ -52,6 +46,6 @@ while (cents > 0)
 	}
 		cents--;
 }
-	printf("%d\n", coins);
-	return (0);
+printf("%d\n", coins);
+return (0);
 }
